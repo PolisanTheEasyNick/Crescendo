1295dc76d4d2462824bb7f76102669b62f1dc599
PolisanTheEasyNick <polisanoleg@gmail.com>
2023-03-23 01:15:06 +0200

    Added simple gui for player

---
+++ playerwindow.h
@@ -0,0 +1,29 @@
+#ifndef PLAYERWINDOW_H
+#define PLAYERWINDOW_H
+
+#include <gtkmm/application.h>
+#include <gtkmm/applicationwindow.h>
+#include <gtkmm/box.h>
+#include <gtkmm/button.h>
+#include <gtkmm/grid.h>
+#include <gtkmm/label.h>
+#include <gtkmm/scale.h>
+#include <gtkmm/volumebutton.h>
+
+class PlayerWindow : public Gtk::ApplicationWindow {
+public:
+  PlayerWindow();
+  virtual ~PlayerWindow(){};
+
+protected:
+  void on_playpause_clicked(), on_prev_clicked(), on_next_clicked(),
+      on_shuffle_clicked(), on_player_choose_clicked();
+  Gtk::Grid main_grid;
+  Gtk::Box control_buttons, volume_and_player;
+  Gtk::Button playpause, prev, next, shuffle, player_choose, device_choose;
+  Gtk::Label song_name;
+  Gtk::Scale progress_bar_song;
+  Gtk::VolumeButton volume_bar;
+};
+
+#endif // PLAYERWINDOW_H
