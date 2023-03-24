0f4a5ff3d6b3575537f426e1dede882f723b4d1d
PolisanTheEasyNick <git@github.com>
2023-03-24 23:02:42 +0200

    Created ability to seek song via Gtk::Scale

--- playerwindow.h
+++ playerwindow.h
@@ -1,10 +1,12 @@
 #ifndef PLAYERWINDOW_H
 #define PLAYERWINDOW_H
 
+#include "player.h"
 #include <gtkmm/application.h>
 #include <gtkmm/applicationwindow.h>
 #include <gtkmm/box.h>
 #include <gtkmm/button.h>
+#include <gtkmm/gestureclick.h>
 #include <gtkmm/grid.h>
 #include <gtkmm/label.h>
 #include <gtkmm/scale.h>
@@ -13,9 +15,10 @@
 class PlayerWindow : public Gtk::ApplicationWindow {
 public:
   PlayerWindow();
-  virtual ~PlayerWindow(){};
+  virtual ~PlayerWindow() {}
 
 protected:
+  Player player;
   void on_playpause_clicked(), on_prev_clicked(), on_next_clicked(),
       on_shuffle_clicked(), on_player_choose_clicked();
   Gtk::Grid main_grid;
